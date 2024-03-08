b565ce0f572116817c820f307cc088d7257eba43
DESKTOP-5BAJO87\Profesor <csi23-csolara@colegioaltair.net>
2024-03-08 09:06:40 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+namespace Ejercicio6
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+
+            Console.WriteLine("Intoduzca una cadena de caracteres: ");
+            string cadena = Console.ReadLine();
+
+            string revertir = Reverse(cadena);
+            Console.WriteLine(revertir);
+        }
+        static string Reverse(string cadena)
+        {
+            char[] charArray = cadena.ToCharArray();
+            string revertir = String.Empty;
+            for (int i = charArray.Length - 1; i >= 0; i--)
+            {
+                revertir += charArray[i];
+            }
+            return revertir;
+        }
+    }
+}
